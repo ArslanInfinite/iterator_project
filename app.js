5be5abd43e754f48779ade363e7bb9e4877ae799
Arslan <ali.arslan319@gmail.com>
2021-10-19 21:56:11 -0400

    tweak html for readability, create array of objects of users in data variable

---
+++ app.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        name: 'John Doe', 
+        age: 32, 
+        gender: 'male', 
+        lookingFor: 'female',
+        location: 'New York City',
+        image: 'https://randomuser.me/api/portraits/men/75.jpg'
+    }, 
+    {
+        name: 'Jen Smith', 
+        age: 30, 
+        gender: 'female', 
+        lookingFor: 'male',
+        location: 'Miami',
+        image: 'https://randomuser.me/api/portraits/women/75.jpg'
+    }, 
+    {
+        name: 'William Johnson', 
+        age: 38, 
+        gender: 'male', 
+        lookingFor: 'female',
+        location: 'Las Vegas',
+        image: 'https://randomuser.me/api/portraits/men/80.jpg'
+    }, 
+]
